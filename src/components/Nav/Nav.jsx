403d3d78bd2455290622aaa8a2d57f3a61143099
Zubairsaififr <juberjubersaifi@gmail.com>
2025-12-29 11:52:47 +0530

    tengo

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -32,9 +32,10 @@ tl.from("nav ul li",{
     <h1>PORTFOLIO</h1>
     <ul className='desktopmenu'>
        <Link to="home" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Home</li></Link>
-       <Link to="ppp" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>About</li> </Link>
-       <Link to="experience" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Experience</li></Link>
+       <Link to="ppp" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>About</li> </Link> 
        <Link to="contact" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Contact</li></Link>
+       <Link to="contact" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Add</li></Link>
+       <Link to="experience" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Experience</li></Link>
        
     </ul>
     <div className="hamburger" ref={menu} onClick={()=>{
@@ -49,8 +50,8 @@ tl.from("nav ul li",{
        <Link to="home" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Home</li></Link>
        <Link to="about" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>About</li> </Link>
        <Link to="experience" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Experience</li></Link>
-       <Link to="contact" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Contact</li></Link>
-       
+       <Link to="contact" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Add</li></Link> 
+       <Link to="contact" activeClass ='active' spy={true} smooth={true} duration={1500}> <li>Contact</li></Link> 
     </ul>
    </nav>
   )
